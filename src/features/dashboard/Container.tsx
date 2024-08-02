bd7d8cdf382af09d781bb3f03d3b9a90f8d191f0
Addo Michael <michaeladdo1997@gmail.com>
2024-08-02 00:14:51 +0000

    updated dashboard

--- src/features/dashboard/Container.tsx
+++ src/features/dashboard/Container.tsx
@@ -1,5 +1,37 @@
-const Container = () => {
-  return <div>container</div>;
+interface PropInterface {
+  title: string;
+  icon: React.ReactNode;
+  num: number;
+  percent: number | React.ReactNode;
+  bgColor: string;
+  color: string;
+}
+
+const Container = ({
+  title,
+  icon,
+  num,
+  percent,
+  bgColor,
+  color,
+}: PropInterface) => {
+  return (
+    <div
+      className={`rounded-lg hover:shadow-lg shadow bg-blue-50 cursor-pointer flex flex-col items-center justify-between pb-4 pt-7 px-5 h-32 w-64`}
+    >
+      <div className="flex items-center gap-16 ">
+        <p className="text-lg font-normal">
+          {percent}
+          {typeof percent === "number" ? "%" : ""}
+        </p>
+        <p className={`text-xl font-semibold ${color} `}>{num}</p>
+      </div>
+      <div className="flex items-center gap-2">
+        <div className={`border ${bgColor} rounded-full p-1`}>{icon}</div>
+        <h1 className="text-lg font-medium uppercase text-blue-950">{title}</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Container;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,16 +1,61 @@
 import Container from "../features/dashboard/Container";
+import { FaCircleUser } from "react-icons/fa6";
+import { MdOutlineReport } from "react-icons/md";
+import { MdDashboard, MdToday } from "react-icons/md";
+import { HiUsers } from "react-icons/hi2";
+import { CgViewMonth } from "react-icons/cg";
 
 const Dashboard = () => {
   return (
-    <div className="py-3 px-10">
-      <h2>Dashboard</h2>
-      <div className="flex justify-between items-center px-10">
-        <Container />
-        <Container />
-        <Container />
-        <Container />
+    <>
+      <div className="flex items-center py-5 gap-2 px-10 ">
+        <div className="p-1 bg-blue-100 rounded-lg">
+          <MdDashboard size={22} />
+        </div>
+        <h2 className="text-2xl font-semibold">Dashboard</h2>
       </div>
-    </div>
+      <div className="pt-5 pb-24 px-3 flex flex-col items-center">
+        <div className="flex flex-wrap justify-between items-center  gap-10">
+          <Container
+            percent={0.02}
+            title="Total"
+            num={100}
+            icon={<MdOutlineReport size={20} color="white" />}
+            bgColor=" bg-red-300"
+            color="text-red-400"
+          />
+          <Container
+            percent={0.02}
+            title="Month"
+            num={20}
+            icon={<CgViewMonth size={15} color="white" />}
+            bgColor="bg-blue-300"
+            color="text-blue-400"
+          />
+          <Container
+            percent={0.02}
+            title="Today"
+            num={50}
+            icon={<MdToday size={15} color="white" />}
+            bgColor="bg-red-400"
+            color="text-red-600"
+          />
+
+          <Container
+            percent={
+              <div className="p-2 bg-blue-950 rounded-full">
+                <FaCircleUser size={15} color="white" />
+              </div>
+            }
+            title="Agents"
+            num={20}
+            icon={<HiUsers size={15} color="white" />}
+            bgColor="bg-blue-950"
+            color="text-blue-900"
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
